3394ba71a7b87e3c6398fb4a32588d2715f40eb6
Brenten Lovato <brentengust@gmail.com>
2015-07-20 14:06:19 -0700

    bp 1

--- TreeMatch.js
+++ TreeMatch.js
@@ -1,8 +1,15 @@
 // nodejs and excel parser for running database tools
 var excelParser = require('excel-parser');
-excelParser.worksheets({
-  inFile: 'my_file.in'
-}, function(err, worksheets){
+ 
+excelParser.parse({
+  inFile: 'my_file.in',
+  worksheet: 1,
+  skipEmpty: true,
+  searchFor: {
+    term: ['my serach term'],
+    type: 'loose'
+  }
+},function(err, records){
   if(err) console.error(err);
-  consol.log(worksheets);
+  consol.log(records);
 });
