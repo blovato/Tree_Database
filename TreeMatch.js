ce733fcbecf39538ffb317ad1067abe680a037bf
Brenten Lovato <brentengust@gmail.com>
2015-07-20 14:04:55 -0700

    boiler plate

--- TreeMatch.js
+++ TreeMatch.js
@@ -1,1 +1,8 @@
 // nodejs and excel parser for running database tools
+var excelParser = require('excel-parser');
+excelParser.worksheets({
+  inFile: 'my_file.in'
+}, function(err, worksheets){
+  if(err) console.error(err);
+  consol.log(worksheets);
+});
